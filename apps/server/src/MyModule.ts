4913572072615ad05d199f5d0fcaa63bf246d257
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2024-12-14 18:55:07 +0900

    nestia 辞める

--- apps/server/src/MyModule.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from "@nestjs/common";
-
-import { BbsArticleModule } from "./controllers/bbs/BbsArticleModule";
-
-@Module({
-  imports: [BbsArticleModule],
-})
-export class MyModule {}
--- apps/server/src/utils/ErrorUtil.ts
+++
@@ -1,9 +0,0 @@
-import serializeError = require("serialize-error");
-
-export namespace ErrorUtil {
-  // biome-ignore lint:
-  export const toJSON = (err: any): object =>
-    err instanceof Object && err.toJSON instanceof Function
-      ? err.toJSON()
-      : serializeError(err);
-}
