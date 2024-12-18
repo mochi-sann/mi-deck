7274e341938a04d5ecce53f6c2f95a18f791d5c1
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2024-12-18 18:28:32 +0900

    react-routeからtanstack/router二移行した

--- apps/front/src/main.tsx
+++ apps/front/src/main.tsx
@@ -2,17 +2,24 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter } from "react-router";
-import App from "./App.tsx";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
 
 const queryClient = new QueryClient();
+import { routeTree } from "./routeTree.gen";
 
+// Create a new router instance
+const router = createRouter({ routeTree });
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>,
 );
--- apps/front/vite.config.ts
+++ apps/front/vite.config.ts
@@ -1,9 +1,9 @@
-import react from "@vitejs/plugin-react-swc";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import viteReact from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [TanStackRouterVite(), viteReact()],
   server: {
     port: 3000,
     proxy: {
