1f6bc01ea9f9b920d7a45e4f80d50d6624ae88e3
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2024-12-26 18:40:23 +0900

    viteのバンドルアナライザーを追加

--- apps/front/vite.config.ts
+++ apps/front/vite.config.ts
@@ -1,10 +1,16 @@
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import viteReact from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import { analyzer } from "vite-bundle-analyzer";
 import tsconfigPaths from "vite-tsconfig-paths";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths({ root: "./" }), TanStackRouterVite(), viteReact()],
+  plugins: [
+    tsconfigPaths({ root: "./" }),
+    TanStackRouterVite(),
+    viteReact(),
+    analyzer(),
+  ],
   server: {
     port: 3000,
     proxy: {
