2fba20dce5f125349a4e5aff5237bb21f7370585
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2024-12-27 23:48:05 +0900

    deleter analyzer

--- apps/front/vite.config.ts
+++ apps/front/vite.config.ts
@@ -1,16 +1,10 @@
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import viteReact from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
-import { analyzer } from "vite-bundle-analyzer";
 import tsconfigPaths from "vite-tsconfig-paths";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tsconfigPaths({ root: "./" }),
-    TanStackRouterVite(),
-    viteReact(),
-    analyzer(),
-  ],
+  plugins: [tsconfigPaths({ root: "./" }), TanStackRouterVite(), viteReact()],
   server: {
     port: 3000,
     proxy: {
