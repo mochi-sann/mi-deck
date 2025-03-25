cb97f3f7d4c753e1344048ae6ae324ed5e040b3d
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2025-03-25 20:51:54 +0900

    style: Reformat database.go with consistent indentation

--- apps/server-go/database/database.go
+++ apps/server-go/database/database.go
@@ -1,52 +1,50 @@
 package database
 
 import (
-  "fmt"
-  "log"
-  "os"
+	"fmt"
+	"log"
+	"os"
+	"server-go/models"
 
-  "gorm.io/driver/postgres"
-  "gorm.io/gorm"
-  "gorm.io/gorm/logger"
-  "server-go/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 var DB *gorm.DB
 
 func InitDB() {
-  dsn := fmt.Sprintf(
-    "host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-    os.Getenv("DB_HOST"),
-    os.Getenv("DB_USER"),
-    os.Getenv("DB_PASSWORD"),
-    os.Getenv("DB_NAME"),
-    os.Getenv("DB_PORT"),
-  )
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+	)
 
-  var err error
-  DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
-    Logger: logger.Default.LogMode(logger.Info),
-  })
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
 
-  if err != nil {
-    log.Fatalf("Failed to connect to database: %v", err)
-  }
+	log.Println("Database connection established")
 
-  log.Println("Database connection established")
+	// Auto migrate all models
+	err = DB.AutoMigrate(
+		&models.User{},
+		&models.UserSetting{},
+		&models.ServerSession{},
+		&models.ServerInfo{},
+		&models.UserInfo{},
+		&models.Panel{},
+	)
+	if err != nil {
+		log.Fatalf("Failed to auto migrate models: %v", err)
+	}
 
-  // Auto migrate all models
-  err = DB.AutoMigrate(
-    &models.User{},
-    &models.UserSetting{},
-    &models.ServerSession{},
-    &models.ServerInfo{},
-    &models.UserInfo{},
-    &models.Panel{},
-  )
-
-  if err != nil {
-    log.Fatalf("Failed to auto migrate models: %v", err)
-  }
-
-  log.Println("Database migration completed")
+	log.Println("Database migration completed")
 }
