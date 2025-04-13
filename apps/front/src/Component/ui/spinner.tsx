eebb9d022008e77f5ae85332e8bc77df432374c9
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2025-04-13 11:09:04 +0900

    feat: use lucide-react LoaderCircle component for spinner

--- apps/front/src/Component/ui/spinner.tsx
+++ apps/front/src/Component/ui/spinner.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { cva, type VariantProps } from "class-variance-authority";
 
-import { cn } from "@/lib/utils";
+import { LoaderCircle } from "lucide-react";
 
 const spinnerVariants = cva("animate-spin text-primary", {
   variants: {
@@ -19,27 +19,14 @@ const spinnerVariants = cva("animate-spin text-primary", {
 
 export interface SpinnerProps
   extends React.SVGAttributes<SVGSVGElement>,
-    VariantProps<typeof spinnerVariants> {}
+  VariantProps<typeof spinnerVariants> { }
 
 const Spinner = React.forwardRef<SVGSVGElement, SpinnerProps>(
   ({ className, size, ...props }, ref) => {
     return (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        className={cn(spinnerVariants({ size }), className)}
-        ref={ref}
-        {...props}
-      >
-        <path d="M21 12a9 9 0 1 1-6.219-8.56" />
-      </svg>
+      <div className="">
+        <LoaderCircle {...props} />
+      </div>
     );
   },
 );
