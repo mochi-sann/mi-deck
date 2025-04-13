5c47b310c136166a0b30c62bf4f00ffbd9438d4f
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2025-04-13 20:02:27 +0900

    feat(AppSidebar.tsx, Layout.tsx): 新しいサイドバーコンポーネントを追加し、レイアウトを更新 削除(Sidebar.tsx): 古いサイドバーコンポーネントを削除し、コードを整理

--- apps/front/src/Component/Layout/Layout.tsx
+++ apps/front/src/Component/Layout/Layout.tsx
@@ -1,22 +1,17 @@
 import type React from "react";
-import { useLocalStorage } from "usehooks-ts";
 import { SidebarProvider } from "../ui/sidebar";
+import { AppSidebar } from "./AppSidebar";
 
 export type LayoutProps = {
   children: React.ReactNode;
 };
 
 export const Layout: React.FC<LayoutProps> = (props) => {
-  const [value, setValue, removeValue] = useLocalStorage(
-    "isSidebarOpen",
-    false,
-  );
+  // const [value, setValue, removeValue] = useLocalStorage("isSidebarOpen", true);
   return (
-    <SidebarProvider defaultOpen={value}>
+    <SidebarProvider>
       <div className="m-0 flex h-svh w-dvw overscroll-none p-0 ">
-        <div className="h-full w-[80px] flex-[0_0_80px] bg-blue-50">
-          <p>side menu</p>
-        </div>
+        <AppSidebar />
         <div className="flex h-svh w-dvw flex-1 overflow-x-auto overflow-y-hidden ">
           <div>{props.children}</div>
         </div>
