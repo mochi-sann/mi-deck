36abf59c76d45bed503d120cfc9f56d3bac443ea
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2025-06-02 19:27:44 +0900

    修正(auth.validator.ts): OpenAPIスキーマの名前を追加

--- apps/hono/src/validators/auth.validator.ts
+++ apps/hono/src/validators/auth.validator.ts
@@ -1,32 +1,36 @@
 import { z } from "zod";
 import "zod-openapi/extend";
 
-export const signUpSchema = z.object({
-  email: z
-    .string()
-    .email({ message: "有効なメールアドレスを入力してください。" })
-    .openapi({ example: "example@example.com", format: "email" }),
-  password: z
-    .string()
-    .min(8, { message: "パスワードは8文字以上である必要があります。" })
-    .openapi({ example: "password123" }),
-  username: z
-    .string()
-    .min(3, { message: "ユーザー名は3文字以上である必要があります。" })
-    .openapi({ example: "username123" }),
-});
+export const signUpSchema = z
+  .object({
+    email: z
+      .string()
+      .email({ message: "有効なメールアドレスを入力してください。" })
+      .openapi({ example: "example@example.com", format: "email" }),
+    password: z
+      .string()
+      .min(8, { message: "パスワードは8文字以上である必要があります。" })
+      .openapi({ example: "password123" }),
+    username: z
+      .string()
+      .min(3, { message: "ユーザー名は3文字以上である必要があります。" })
+      .openapi({ example: "username123" }),
+  })
+  .openapi("SignUpInput");
 
 export type SignUpInput = z.infer<typeof signUpSchema>;
 
-export const loginSchema = z.object({
-  email: z
-    .string()
-    .email({ message: "有効なメールアドレスを入力してください。" })
-    .openapi({ example: "example@example.com" }),
-  password: z
-    .string()
-    .min(1, { message: "パスワードを入力してください。" })
-    .openapi({ example: "password" }),
-});
+export const loginSchema = z
+  .object({
+    email: z
+      .string()
+      .email({ message: "有効なメールアドレスを入力してください。" })
+      .openapi({ example: "example@example.com" }),
+    password: z
+      .string()
+      .min(1, { message: "パスワードを入力してください。" })
+      .openapi({ example: "password" }),
+  })
+  .openapi("LoginInput");
 
 export type LoginInput = z.infer<typeof loginSchema>;
