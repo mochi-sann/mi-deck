ea4bd2c6bd4c512a6fabdc355d90c0bb9c4b85cf
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2025-06-02 14:19:00 +0900

    feat(api): 新しいAPIエンドポイントとOpenAPI仕様を追加 fix(.tool-versions): Node.jsのバージョンを22.15.0から22.14.0に変更 feat(go): Goモジュールと生成スクリプトを追加 feat(yml): petstore.ymlファイルを追加し、ペットストアのスキーマを定義 feat(swagger): Swagger仕様を追加し、APIのエンドポイントを文書化

---
+++ apps/api/generater.go
@@ -0,0 +1,5 @@
+package project
+
+
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen@latest --target petstore --clean petstore.yml
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen@latest --target api --clean ./swagger.yml
