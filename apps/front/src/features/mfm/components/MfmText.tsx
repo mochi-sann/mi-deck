3f2efa97b6db34165714f11f70b74031b223975e
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2025-07-14 22:31:56 +0900

    オーバーフローを修正

--- apps/front/src/features/mfm/components/MfmText.tsx
+++ apps/front/src/features/mfm/components/MfmText.tsx
@@ -12,7 +12,9 @@ interface MfmTextProps {
 export function MfmText({ text, host, emojis }: MfmTextProps) {
   return (
     <Fragment>
-      <Mfm text={text} host={host} emojis={emojis} />
+      <div className="max-w-full">
+        <Mfm text={text} host={host} emojis={emojis} />
+      </div>
     </Fragment>
   );
 }
