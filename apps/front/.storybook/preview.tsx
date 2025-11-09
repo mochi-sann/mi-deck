e5cd0a87f0965bcd54d1930fddbc9a0beee5b556
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2025-11-09 17:23:06 +0900

    chore: Storybook に Providers を適用

---
+++ apps/front/.storybook/preview.tsx
@@ -0,0 +1,27 @@
+import type { Preview } from "@storybook/react-vite";
+import { Providers } from "../src/providers";
+import "../src/index.css"; // Import global CSS
+
+const preview: Preview = {
+  decorators: [
+    (Story) => (
+      <Providers>
+        <Story />
+      </Providers>
+    ),
+  ],
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+
+    docs: {
+      codePanel: true,
+    },
+  },
+};
+
+export default preview;
