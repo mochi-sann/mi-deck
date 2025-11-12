2393993e5d0fc262248b4033ea6da4b386429fc8
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2025-11-12 16:41:55 +0900

    triggerを戻した

--- apps/front/src/components/layout/AppSidebar.tsx
+++ apps/front/src/components/layout/AppSidebar.tsx
@@ -1,6 +1,7 @@
 import { Pen } from "lucide-react";
 import { useTranslation } from "react-i18next";
 import { NewNote } from "@/features/notes";
+import { cn } from "@/lib/utils";
 import { Avatar, AvatarFallback } from "../ui/avatar";
 import {
   Sidebar,
@@ -8,19 +9,29 @@ import {
   SidebarFooter,
   SidebarMenuButton,
   SidebarTrigger,
+  useSidebar,
 } from "../ui/sidebar";
 import { NavContent } from "./Sidebar/nav-content";
 
 export const AppSidebarPresenter = () => {
   const { t } = useTranslation("common");
+  const { state } = useSidebar();
 
   return (
     <Sidebar collapsible="icon">
+      <SidebarTrigger
+        className={cn(
+          "-translate-y-1/2 absolute top-[calc(var(--sidebar-width-icon)-1.5rem)] z-50 transition-all duration-200 ease-linear",
+          state === "expanded"
+            ? "left-[calc(var(--sidebar-width)-2.5rem)]"
+            : "left-[calc(var(--sidebar-width-icon)-2.5rem)]",
+        )}
+      />
+      <div className="h-8" />
       <SidebarContent>
         <NavContent />
       </SidebarContent>
       <SidebarFooter>
-        <SidebarTrigger />
         <NewNote
           trigger={
             <SidebarMenuButton
