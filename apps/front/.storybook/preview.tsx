dd8d8b577f9036d22c9cf498d07226dbf2559985
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2025-12-16 23:22:16 +0900

    storybookのテーマ切り替えを追加

--- apps/front/.storybook/preview.tsx
+++ apps/front/.storybook/preview.tsx
@@ -1,21 +1,40 @@
 import type { Preview } from "@storybook/react-vite";
 import { Providers } from "../src/providers";
+import "katex/dist/katex.min.css";
+import "mfm-react-render/style.css";
 import "../src/index.css"; // Import global CSS
-import React from "react";
 
 const preview: Preview = {
   decorators: [
-    (Story) => (
-      <Providers>
-        <Story />
-      </Providers>
-    ),
+    (Story, context) => {
+      const isDark = context.globals.backgrounds?.value === "dark";
+      const html = document.documentElement;
+      if (isDark) {
+        html.classList.add("dark");
+      } else {
+        html.classList.remove("dark");
+      }
+      return (
+        <Providers>
+          <Story />
+        </Providers>
+      );
+    },
   ],
+  initialGlobals: {
+    backgrounds: { value: "light" },
+  },
   parameters: {
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
+      },
+    },
+    backgrounds: {
+      options: {
+        light: { name: "light", value: " oklch(1 0 0)" }, // ライトテーマ用背景
+        dark: { name: "dark", value: "oklch(0.129 0.042 264.695)" }, // ダークテーマ用背景
       },
     },
 
